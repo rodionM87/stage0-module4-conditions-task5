001ee3eae57e55454cbe9cfc4460526edf2dc29b
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-21 23:31:44 +0600

    Update RangeNumbersPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task5;
 
 public class RangeNumbersPrinter {
     public void printRange(int number) {
+        switch((1<=number&&number<=5)?0:(6<=number&&number<=10)?1:2) {
+            case 0:
+                System.out.println("number is between 1 and 5");
+                break;
+            case 1:
+                System.out.println("number is between 6 and 10");
+                break;
+            case 2:
+                System.out.println("out of range");
+                break;
     }
 }
